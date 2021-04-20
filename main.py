3cf5ea6033850ab6091ad057546c1cfdcd17e11e
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-04-21 00:40:12 +0200

    homework done

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import json
+
 from fastapi import FastAPI, Response
 from starlette.requests import Request
 import hashlib
+from pydantic import BaseModel
+from datetime import datetime, timedelta
 
 app = FastAPI()
 app.counter = 0
+app.patient_id = 1
+app.registered = {}
 
 
 """----------------------------------------------------Course--------------------------------------------------------"""
@@ -59,6 +65,41 @@ async def authorize(password: str, password_hash: str, response: Response):
         response.status_code = 204
     else:
         response.status_code = 401
+
+
+class PatientRegistration(BaseModel):
+    name: str
+    surname: str
+
+
+@app.post('/register/')
+async def register_patient(patient: PatientRegistration, response: Response):
+    response.status_code = 201
+    vaccination_date = (datetime.today() + timedelta(len(patient.name) + len(patient.surname))).strftime('%Y-%m-%d')
+    return_val = {
+        "id": app.patient_id,
+        "name": str(patient.name),
+        "surname": str(patient.surname),
+        "register_date": str(datetime.today().strftime('%Y-%m-%d')),
+        "vaccination_date": str(vaccination_date)
+    }
+    app.registered[app.patient_id] = return_val
+    app.patient_id += 1
+    return json.dumps(return_val)
+
+
+@app.get('/patient/{id}')
+async def get_patient_info(id: int, response: Response):
+    patient_info = app.registered.get(id, False)
+    if not patient_info:
+        if id < 1:
+            response.status_code = 400
+        else:
+            response.status_code = 404
+    else:
+        response.status_code = 200
+        return json.dumps(patient_info)
+
 
 
 
