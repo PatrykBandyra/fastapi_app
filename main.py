f9bc5fb8fe23ceb4d7ba0b68dddf942f38e8c80a
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-04-20 23:50:00 +0200

    auth added

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from starlette.requests import Request
+import hashlib
 
 app = FastAPI()
 app.counter = 0
+
+
+"""----------------------------------------------------Course--------------------------------------------------------"""
+
+
+@app.get('/hello/{name}')
+def hello_name_view(name: str):
+    return f'Hello {name}'
+
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+
+"""----------------------------------------------------Homework------------------------------------------------------"""
 
 
 @app.get('/', status_code=200)
@@ -11,7 +30,7 @@ def root():
 
 @app.get('/method', status_code=200)
 def return_method_get():
-    return {"method": "GET"}
+    return {'method': 'GET'}
 
 
 @app.post('/method', status_code=201)
@@ -26,7 +45,7 @@ def return_method_delete():
 
 @app.put('/method', status_code=200)
 def return_method_put():
-    return {'method': 'PUT'}
+    return {"method": "PUT"}
 
 
 @app.options('/method', status_code=200)
@@ -34,13 +53,15 @@ def return_method_options():
     return {'method': 'OPTIONS'}
 
 
+@app.get('/auth/')
+async def authorize(password: str, password_hash: str, response: Response):
+    if hashlib.sha512(bytes(password, encoding='utf-8')).hexdigest() == password_hash:
+        response.status_code = 204
+    else:
+        response.status_code = 401
 
-@app.get('/hello/{name}')
-def hello_name_view(name: str):
-    return f'Hello {name}'
 
 
-@app.get('/counter')
-def counter():
-    app.counter += 1
-    return str(app.counter)
+
+
+
