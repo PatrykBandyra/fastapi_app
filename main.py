c2da2e5e0dc3ed10e00a3de31e590759b3918d8c
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-04-20 20:42:09 +0200

    main.py modified

--- main.py
+++ main.py
@@ -9,9 +9,29 @@ def root():
     return {'message': 'Hello World'}
 
 
-@app.get('/method')
-def return_request_method():
-    pass
+@app.get('/method', status_code=200)
+def return_method_get():
+    return {'method': 'GET'}
+
+
+@app.post('/method', status_code=201)
+def return_method_post():
+    return {'method': 'POST'}
+
+
+@app.delete('/method', status_code=200)
+def return_method_delete():
+    return {'method': 'DELETE'}
+
+
+@app.put('/method', status_code=200)
+def return_method_put():
+    return {'method': 'PUT'}
+
+
+@app.options('/method', status_code=200)
+def return_method_options():
+    return {'method': 'OPTIONS'}
 
 
 
